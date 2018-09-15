51ff481db16ae815bbb5db97185e392264430268
Michael Babker <michael.babker@gmail.com>
2018-09-15 00:09:10 -0500

    Add some more content to the homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,4 +9,18 @@
             <h2 class="hero-subtitle">Creating Open Source Solutions Since 2010</h2>
         </div>
     </section>
+    <section class="homepage-callouts">
+        <div class="callout callout__title callout--who-i-am">
+            <h3 class="callout__heading">Who I Am</h3>
+        </div>
+        <div class="callout callout__text callout--who-i-am-info">
+            I am <a href="https://michaels.website" rel="nofollow noopener">Michael Babker</a>, a long tenured contributor to the <a href="https://www.joomla.org" rel="nofollow noopener">Joomla!</a> project and Lead Developer for <a href="https://hdwebpros.com" rel="nofollow noopener">Happy Dog Web Productions</a>.
+        </div>
+        <div class="callout callout__title callout--what-i-do">
+            <h3 class="callout__heading">What I Do</h3>
+        </div>
+        <div class="callout callout__text callout--what-i-do-info">
+            As an open source contributor and developer, I <a href="#">create solutions</a> to help others fulfill their own requirements, and I <a href="https://volunteers.joomla.org/joomlers/michael-babker" rel="nofollow noopener">participate heavily</a> in the Joomla! community as a "core maintainer" and release lead. As the Lead Developer of Happy Dog Web Productions, I work closely with clients to create solutions tailored to their business needs.
+        </div>
+    </section>
 @endsection
