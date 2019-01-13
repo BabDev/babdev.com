40de6c0b50ad0bff4bf3a532db09cf6248363492
Michael Babker <michael.babker@gmail.com>
2019-01-12 19:22:15 -0600

    Update NPM deps

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,5 @@
 try {
-    window.$ = window.jQuery = require('jquery');
-
+    require('jquery');
     require('bootstrap/js/dist/util');
     require('bootstrap/js/dist/collapse');
 } catch (e) {}
