dc2e3ec5922d92d2091e7dd93ca9bfc728814e02
Michael Babker <michael.babker@gmail.com>
2019-01-20 19:50:36 -0600

    Add Telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,21 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        // Limit database key length
+        Schema::defaultStringLength(191);
+    }
+
     public function register()
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 }
