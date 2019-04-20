43a4d028fedeb35950aea3fab87c63066e5eab6f
Michael Babker <michael.babker@gmail.com>
2019-04-20 18:40:55 -0500

    NPM update

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,21 @@ const mix = require('laravel-mix');
  |
  */
 
+// Fix for resolve-url-loader error regarding paths
+Mix.listen('configReady', (config) => {
+    for (rule of config.module.rules) {
+        if (new RegExp("\.scss$").test(rule.test.toString())) {
+            let resolveUrlLoaderIndex = 0;
+            rule.use.forEach(function (element, index) {
+                if (element.loader === "resolve-url-loader") {
+                    resolveUrlLoaderIndex = index;
+                }
+            });
+            rule.use.splice(resolveUrlLoaderIndex - 1, 0, rule.use.splice(resolveUrlLoaderIndex, 1)[0]);
+        }
+    }
+});
+
 mix.js('resources/js/app.js', 'public/js')
     .extract(['jquery', 'bootstrap/js/dist/util', 'bootstrap/js/dist/collapse']);
 
