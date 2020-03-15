e4c490d71c03b52cd49328e2447291f6adddd4dc
Michael Babker <michael.babker@gmail.com>
2020-03-15 17:25:32 -0500

    Dump old stuff

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -1,8 +1,7 @@
 <?php
 
-use BabDev\Models\JoomlaExtension;
-use BabDev\Models\JoomlaExtensionRelease;
 use BabDev\Models\Package;
+use BabDev\Models\PackageRelease;
 use DaveJamesMiller\Breadcrumbs\BreadcrumbsGenerator;
 use DaveJamesMiller\Breadcrumbs\BreadcrumbsManager;
 
@@ -24,32 +23,6 @@ $breadcrumbs->for(
 );
 
 $breadcrumbs->for(
-    'joomla-extensions.index',
-    static function (BreadcrumbsGenerator $trail) {
-        $trail->parent('homepage');
-        $trail->push('Open Source');
-        $trail->push('Joomla! Extensions', route('joomla-extensions.index'));
-    }
-);
-
-$breadcrumbs->for(
-    'joomla-extensions.releases.index',
-    static function (BreadcrumbsGenerator $trail, JoomlaExtension $extension) {
-        $trail->parent('joomla-extensions.index');
-        $trail->push($extension->name);
-        $trail->push('Releases', route('joomla-extensions.releases.index', ['joomla_extension' => $extension]));
-    }
-);
-
-$breadcrumbs->for(
-    'joomla-extensions.releases.show',
-    static function (BreadcrumbsGenerator $trail, JoomlaExtension $extension, JoomlaExtensionRelease $release) {
-        $trail->parent('joomla-extensions.releases.index', $extension);
-        $trail->push($release->version, route('joomla-extensions.releases.show', ['joomla_extension' => $extension, 'joomla_extension_release' => $release]));
-    }
-);
-
-$breadcrumbs->for(
     'open-source.packages',
     static function (BreadcrumbsGenerator $trail) {
         $trail->parent('homepage');
@@ -64,5 +37,13 @@ $breadcrumbs->for(
         $trail->parent('open-source.packages');
         $trail->push($package->name);
         $trail->push('Releases', route('open-source.package.releases', ['package' => $package]));
+    }
+);
+
+$breadcrumbs->for(
+    'open-source.package.release',
+    static function (BreadcrumbsGenerator $trail, Package $package, PackageRelease $release) {
+        $trail->parent('open-source.package.releases', $package);
+        $trail->push($release->version, route('open-source.package.release', ['package' => $package, 'package_release' => $release]));
     }
 );
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use BabDev\Http\Controllers\JoomlaExtensionReleasesController;
-use BabDev\Http\Controllers\JoomlaExtensionsController;
 use BabDev\Http\Controllers\OpenSourceController;
 use Illuminate\Routing\Router;
 
@@ -19,18 +17,3 @@ $router->get(
     '/open-source/packages/{package}/releases',
     [OpenSourceController::class, 'packageReleases']
 )->name('open-source.package.releases');
-
-$router->get(
-    '/joomla-extensions',
-    [JoomlaExtensionsController::class, 'index']
-)->name('joomla-extensions.index');
-
-$router->get(
-    '/joomla-extensions/{joomla_extension}/releases',
-    [JoomlaExtensionReleasesController::class, 'index']
-)->name('joomla-extensions.releases.index');
-
-$router->get(
-    '/joomla-extensions/{joomla_extension}/release/{joomla_extension_release}',
-    [JoomlaExtensionReleasesController::class, 'show']
-)->name('joomla-extensions.releases.show');
