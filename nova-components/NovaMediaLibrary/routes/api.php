5bc39d35fd8c1dd25700551067bf67e57994ac65
Michael Babker <michael.babker@gmail.com>
2020-03-21 21:34:16 -0500

    Will have routing

---
+++ nova-components/NovaMediaLibrary/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Tool API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you may register API routes for your tool. These routes
+| are loaded by the ServiceProvider of your tool. They are protected
+| by your tool's "Authorize" middleware by default. Now, go build!
+|
+*/
--- nova-components/NovaMediaLibrary/src/FieldServiceProvider.php
+++ nova-components/NovaMediaLibrary/src/FieldServiceProvider.php
@@ -2,32 +2,30 @@
 
 namespace BabDev\NovaMediaLibrary;
 
+use Illuminate\Auth\Middleware\Authorize;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Nova\Events\ServingNova;
 use Laravel\Nova\Nova;
 
 class FieldServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
+        $this->app->booted(static function (Application $app) {
+            if ($app->routesAreCached()) {
+                return;
+            }
+
+            Route::middleware(['nova', Authorize::class])
+                ->prefix('nova-vendor/babdev/-nova-media-library')
+                ->group(__DIR__.'/../routes/api.php');
+        });
+
         Nova::serving(static function (ServingNova $event) {
             Nova::script('nova-media-library', __DIR__ . '/../dist/js/field.js');
             Nova::style('nova-media-library', __DIR__ . '/../dist/css/field.css');
         });
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
     }
 }
