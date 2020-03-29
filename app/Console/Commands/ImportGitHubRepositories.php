05386fdbf34e6a2496b1c5d84ea517819c2a6d84
Michael Babker <michael.babker@gmail.com>
2020-03-29 16:08:56 -0500

    Don't need cache here

--- app/Console/Commands/ImportGitHubRepositories.php
+++ app/Console/Commands/ImportGitHubRepositories.php
@@ -5,7 +5,6 @@ namespace BabDev\Console\Commands;
 use BabDev\GitHub\ApiConnector;
 use BabDev\Models\Package;
 use Illuminate\Console\Command;
-use Illuminate\Contracts\Cache\Repository as CacheContract;
 
 class ImportGitHubRepositories extends Command
 {
@@ -14,12 +13,10 @@ class ImportGitHubRepositories extends Command
     protected $description = 'Import GitHub repositories to the application.';
 
     private ApiConnector $github;
-    private CacheContract $cache;
 
-    public function __construct(ApiConnector $github, CacheContract $cache)
+    public function __construct(ApiConnector $github)
     {
         $this->github = $github;
-        $this->cache = $cache;
 
         parent::__construct();
     }
@@ -38,13 +35,7 @@ class ImportGitHubRepositories extends Command
                         'name' => $repositoryAttributes['name'],
                         'display_name' => \ucwords(\str_replace(['-', '_'], ' ', $repositoryAttributes['name'])),
                         'description' => $repositoryAttributes['description'],
-                        'topics' => $this->cache->remember(
-                            "github-repository_topics-{$repositoryAttributes['name']}",
-                            3600,
-                            function () use ($repositoryAttributes) {
-                                return $this->github->fetchRepositoryTopics('BabDev', $repositoryAttributes['name']);
-                            }
-                        ),
+                        'topics' => $this->github->fetchRepositoryTopics('BabDev', $repositoryAttributes['name']),
                         'stars' => $repositoryAttributes['stargazers_count'],
                         'language' => $repositoryAttributes['language'],
                         'supported' => $repositoryAttributes['archived'] === false,
