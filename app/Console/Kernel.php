f05e2e91b722c609d0c6e4d5c723f1471d9c88b5
Michael Babker <michael.babker@gmail.com>
2020-03-31 18:57:34 -0500

    Wire up Trix attachments integration

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,12 +2,19 @@
 
 namespace BabDev\Console;
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Laravel\Nova\Trix\PruneStaleAttachments;
 
 class Kernel extends ConsoleKernel
 {
     protected function commands()
     {
         $this->load(__DIR__ . '/Commands');
+    }
+
+    protected function schedule(Schedule $schedule)
+    {
+        $schedule->call(new PruneStaleAttachments())->daily();
     }
 }
