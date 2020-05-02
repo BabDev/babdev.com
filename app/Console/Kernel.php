1b969203f4af0012e635e5f0e7348c3de7bcaff5
Michael Babker <michael.babker@gmail.com>
2020-05-01 23:26:27 -0500

    Remove schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,19 +2,12 @@
 
 namespace BabDev\Console;
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Laravel\Nova\Trix\PruneStaleAttachments;
 
 class Kernel extends ConsoleKernel
 {
     protected function commands()
     {
         $this->load(__DIR__ . '/Commands');
-    }
-
-    protected function schedule(Schedule $schedule)
-    {
-        $schedule->call(new PruneStaleAttachments())->daily();
     }
 }
