a07e60ee64b987457da809681833c8122edcba1e
Michael Babker <michael.babker@gmail.com>
2020-05-03 14:16:15 -0500

    Relation no longer exists

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -4,34 +4,30 @@ namespace BabDev\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
 /**
- * @property int                              $id
- * @property string                           $name
- * @property string                           $display_name
- * @property string|null                      $packagist_name
- * @property string                           $slug
- * @property string|null                      $logo
- * @property string|null                      $description
- * @property array|null                       $topics
- * @property string                           $documentation_type
- * @property string|null                      $package_type
- * @property int                              $stars
- * @property int|null                         $downloads
- * @property string|null                      $language
- * @property bool                             $supported
- * @property bool                             $visible
- * @property bool                             $is_packagist
- * @property Carbon|null                      $created_at
- * @property Carbon|null                      $updated_at
- * @property-read string                      $github_url
- * @property-read Collection|PackageRelease[] $releases
- * @property-read int|null                    $releases_count
+ * @property int         $id
+ * @property string      $name
+ * @property string      $display_name
+ * @property string|null $packagist_name
+ * @property string      $slug
+ * @property string|null $logo
+ * @property string|null $description
+ * @property array|null  $topics
+ * @property string      $documentation_type
+ * @property string|null $package_type
+ * @property int         $stars
+ * @property int|null    $downloads
+ * @property string|null $language
+ * @property bool        $supported
+ * @property bool        $visible
+ * @property bool        $is_packagist
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read string $github_url
  *
  * @method static Builder|Package isPackagist()
  * @method static Builder|Package query()
@@ -87,11 +83,6 @@ class Package extends Model
     public function scopeVisible(Builder $query): Builder
     {
         return $query->where('visible', '=', true);
-    }
-
-    public function releases(): HasMany
-    {
-        return $this->hasMany(PackageRelease::class);
     }
 
     public function getGithubUrlAttribute(): string
