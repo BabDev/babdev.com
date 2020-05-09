95ed1f2380c910eaf5e1d6039e46d8681895652f
Michael Babker <michael.babker@gmail.com>
2020-05-09 15:24:41 -0500

    Use package title block on update page

--- resources/views/open_source/updates/show.blade.php
+++ resources/views/open_source/updates/show.blade.php
@@ -5,10 +5,15 @@
 @section('title', sprintf('%s | Open Source Updates | %s', $update->title, config('app.name', 'Laravel')))
 
 @section('content')
-    <header class="hero">
-        <div class="hero__text">
-            <h1 class="hero__title">{{ $update->package->display_name }}</h1>
-            <h2 class="hero__subtitle">Package Update</h2>
+    <header class="package-title{{ $update->package->logo ? ' package-title--has-logo' : '' }} pt-4">
+        @if($update->package->logo)
+            <div class="package-title__logo">
+                <img src="{{ Storage::disk('logos')->url($update->package->logo) }}" alt="{{ $update->package->display_name }} Logo">
+            </div>
+        @endif
+        <div class="package-title__name">
+            <h1 class="package-title__primary">{{ $update->package->display_name }}</h1>
+            <h2 class="package-title__secondary">Package Update</h2>
         </div>
     </header>
     <article class="pt-4">
