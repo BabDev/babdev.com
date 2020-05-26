e8bf601d029b2fd4c5f87bbb16efd8fedbd08b10
Michael Babker <michael.babker@gmail.com>
2020-05-25 22:40:42 -0500

    Note IP handling

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -15,6 +15,13 @@
             </div>
             <p>This site is designed with privacy in mind and tends to take a privacy first approach.</p>
             <div class="section-heading">
+                <h2>Collected Information</h2>
+            </div>
+            <p>Some potentially identifying information is collected as a result of visiting this website. This information, and the purpose of its collection, includes:</p>
+            <ul>
+                <li>IP Address - Your IP address is collected and stored in the web server's logs and security tools as a means of ensuring the security of this website and preventing abuse.</li>
+            </ul>
+            <div class="section-heading">
                 <h2>External Services</h2>
             </div>
             <p>There are some integrations with third party services to improve the capabilities of this site.</p>
