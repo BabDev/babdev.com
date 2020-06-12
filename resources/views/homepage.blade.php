aec6a089e3402515e7bdff850fe28f12a6a8f79a
Michael Babker <michael.babker@gmail.com>
2020-06-11 22:20:49 -0500

    Don't skip h2

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -12,13 +12,13 @@
     </section>
     <section class="homepage-callouts">
         <div class="callout callout__title callout--who-i-am">
-            <h3 class="callout__heading">Who I Am</h3>
+            <h2 class="callout__heading">Who I Am</h2>
         </div>
         <div class="callout callout__text callout--who-i-am-info">
             I am <a href="https://michaels.website" rel="nofollow noopener">Michael Babker</a>, a long tenured contributor to the Open Source software community and Lead Developer for <a href="https://hdwebpros.com" rel="nofollow noopener">Happy Dog Web Productions</a>.
         </div>
         <div class="callout callout__title callout--what-i-do">
-            <h3 class="callout__heading">What I Do</h3>
+            <h2 class="callout__heading">What I Do</h2>
         </div>
         <div class="callout callout__text callout--what-i-do-info">
             As an open source contributor and developer, I create solutions to help others fulfill their own requirements for PHP projects (primarily with the Symfony and Laravel frameworks). As the Lead Developer of Happy Dog Web Productions, I work closely with clients to create solutions tailored to their business needs.
