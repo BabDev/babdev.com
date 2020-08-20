de0923b748502e6673dff2d1daf3b3246d2b4ff8
Michael Babker <michael.babker@gmail.com>
2020-08-19 19:32:57 -0500

    Add Matomo analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,20 @@
         @yield('meta')
         <title>{{ $title ?? config('app.name', 'BabDev') }}</title>
         <link href="{{ PushManager::preload(mix('css/app.css'), ['as' => 'style']) }}" rel="stylesheet">
+        @if(request()->getHost() === 'www.babdev.com')
+            <script type="text/javascript">
+                var _paq = window._paq = window._paq || [];
+                _paq.push(['trackPageView']);
+                _paq.push(['enableLinkTracking']);
+                (function() {
+                    var u="https://babdev.matomo.cloud/";
+                    _paq.push(['setTrackerUrl', u+'matomo.php']);
+                    _paq.push(['setSiteId', '1']);
+                    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                    g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/babdev.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+                })();
+            </script>
+        @endif
     </head>
     <body>
         <header class="sticky-top">
