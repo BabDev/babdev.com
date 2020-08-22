be9085bd13a86e75b8ff0b97a4305daa9b1b369e
Michael Babker <michael.babker@gmail.com>
2020-08-22 11:32:41 -0500

    Tweak dashboard

--- app/Nova/Dashboards/MatomoAnalytics.php
+++ app/Nova/Dashboards/MatomoAnalytics.php
@@ -4,11 +4,9 @@ namespace BabDev\Nova\Dashboards;
 
 use Laravel\Nova\Dashboard;
 use Rocramer\MatomoAnalytics\Cards\BounceRate;
-use Rocramer\MatomoAnalytics\Cards\Downloads;
 use Rocramer\MatomoAnalytics\Cards\EntryPages;
 use Rocramer\MatomoAnalytics\Cards\ExitPages;
 use Rocramer\MatomoAnalytics\Cards\MostViewedPages;
-use Rocramer\MatomoAnalytics\Cards\Outlinks;
 use Rocramer\MatomoAnalytics\Cards\UniqueVisitors;
 use Rocramer\MatomoAnalytics\Cards\VisitLength;
 use Rocramer\MatomoAnalytics\Cards\Visits;
@@ -23,12 +21,10 @@ class MatomoAnalytics extends Dashboard
     public function cards()
     {
         return [
-            new UniqueVisitors(),
-            new Visits(),
-            new VisitLength(),
-            new BounceRate(),
-            new Outlinks(),
-            new Downloads(),
+            (new UniqueVisitors())->width('1/2'),
+            (new Visits())->width('1/2'),
+            (new VisitLength())->width('1/2'),
+            (new BounceRate())->width('1/2'),
             new EntryPages(),
             new ExitPages(),
             new MostViewedPages(),
