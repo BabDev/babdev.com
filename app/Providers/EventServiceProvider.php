f299c0d4501b7f97aa63f71d72fa1ec1f2f8ffed
Michael Babker <michael.babker@gmail.com>
2020-08-23 12:48:40 -0500

    Use event auto-discovery

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,24 +2,12 @@
 
 namespace BabDev\Providers;
 
-use BabDev\Breadcrumbs\Events\AfterBreadcrumbGenerated;
-use BabDev\Listeners\AppendPageNumberToBreadcrumbs;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
-    protected $listen = [
-        /*
-         * BabDev events - Breadcrumbs package
-         */
-
-        AfterBreadcrumbGenerated::class => [
-            AppendPageNumberToBreadcrumbs::class,
-        ],
-    ];
+    public function shouldDiscoverEvents(): bool
+    {
+        return true;
+    }
 }
