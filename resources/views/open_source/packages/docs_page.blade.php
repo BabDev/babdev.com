e2686616524e5f9d19d776095088b510ee89a953
Michael Babker <michael.babker@gmail.com>
2021-01-01 11:36:28 -0600

    When a package has multiple versions, display a version selector on the docs page to jump between versions (Fixes #9)

--- resources/views/open_source/packages/docs_page.blade.php
+++ resources/views/open_source/packages/docs_page.blade.php
@@ -13,7 +13,22 @@
     <x-package-title :package="$package" secondary-title="Documentation" class="pt-4" />
     <article class="pt-4">
         <div class="container-fluid package-docs">
-            <x-markdown class="package-docs__sidebar" flavor="github">{!! $sidebar !!}</x-markdown>
+            <div class="package-docs__sidebar">
+                @if(count($package->docs_branches) > 1)
+                    <div class="package-docs__version-selector dropdown">
+                        <a class="btn btn-light dropdown-toggle" href="#" role="button" id="docs-version-selector" data-bs-toggle="dropdown" aria-expanded="false">
+                            Select Version ({{ $version }})
+                        </a>
+
+                        <ul class="dropdown-menu" aria-labelledby="docs-version-selector">
+                            @foreach($package->docs_branches as $docsVersion => $docsBranch)
+                                <li><a class="dropdown-item{{ $docsVersion === $version ? ' active' : '' }}" href="{{ route('open-source.packages.package-docs-page', ['package' => $package, 'version' => $docsBranch, 'slug' => $slug]) }}">{{ $docsVersion }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <x-markdown flavor="github">{!! $sidebar !!}</x-markdown>
+            </div>
             <x-markdown class="package-docs__content" flavor="github">{!! $contents !!}</x-markdown>
         </div>
         <div class="container">
