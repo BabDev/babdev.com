8822000a25423e7ebcfaa7b86a811f72a2261e1c
Michael Babker <michael.babker@gmail.com>
2021-01-10 17:27:58 -0600

    Test the GitHub importer

---
+++ tests/Feature/ImportGitHubRepositoriesTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature;
+
+use BabDev\GitHub\ApiConnector;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Mockery\MockInterface;
+use Tests\TestCase;
+
+class ImportGitHubRepositoriesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function the_public_repositories_are_imported()
+    {
+        $this->instance(
+            ApiConnector::class,
+            \Mockery::mock(ApiConnector::class, function (MockInterface $mock) {
+                $mock->shouldReceive('fetchPublicRepositories')->once()->andReturn(
+                    collect(
+                        [
+                            [
+                                'name' => 'babdev.com',
+                                'description' => 'The babdev.com source code',
+                                'stargazers_count' => 1,
+                                'language' => 'PHP',
+                                'archived' => false,
+                            ],
+                            [
+                                'name' => 'Pagerfanta',
+                                'description' => 'Pagination library for PHP applications with support for several data providers',
+                                'stargazers_count' => 144,
+                                'language' => 'PHP',
+                                'archived' => false,
+                            ],
+                            [
+                                'name' => 'Podcast-Manager',
+                                'description' => 'Podcast Manager is a suite of extensions allowing users to host and manage a podcast feed from their Joomla! site.',
+                                'stargazers_count' => 39,
+                                'language' => 'PHP',
+                                'archived' => true,
+                            ],
+                        ]
+                    )
+                );
+                $mock->shouldReceive('fetchRepositoryTopics')->twice()->andReturn(collect());
+            })
+        );
+
+        $this->artisan('import:github-repositories')
+            ->assertExitCode(0);
+
+        $this->assertDatabaseCount('packages', 2);
+    }
+}
