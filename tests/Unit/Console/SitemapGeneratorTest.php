718819dcb997864465730c3023527f09b71f7ef8
Michael Babker <michael.babker@gmail.com>
2021-01-10 17:19:15 -0600

    Test the sitemap generator command

---
+++ tests/Unit/Console/SitemapGeneratorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit\Console;
+
+use Illuminate\Support\Facades\Storage;
+use Mockery\MockInterface;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\SitemapGenerator;
+use Tests\TestCase;
+
+class SitemapGeneratorTest extends TestCase
+{
+    /** @test */
+    public function the_sitemap_is_generated()
+    {
+        Storage::fake('local');
+
+        $this->instance(
+            SitemapGenerator::class,
+            \Mockery::mock(SitemapGenerator::class, function (MockInterface $mock) {
+                $mock->shouldReceive('setUrl')->once()->andReturnSelf();
+                $mock->shouldReceive('hasCrawled')->once()->andReturnSelf();
+                $mock->shouldReceive('getSitemap')->once()->andReturn(Sitemap::create());
+            })
+        );
+
+        $this->artisan('sitemap:generate')
+            ->assertExitCode(0);
+
+        Storage::disk('local')->assertExists('sitemap.xml');
+    }
+}
