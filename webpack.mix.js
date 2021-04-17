0ff35374dacf3c75e9605e89c341a15bd6376e23
Michael Babker <michael.babker@gmail.com>
2021-04-16 22:05:04 -0500

    Bump NPM deps

--- webpack.mix.js
+++ webpack.mix.js
@@ -25,7 +25,7 @@ mix.js('resources/js/app.js', 'public/js')
                     modules: 'auto',
                     forceAllTransforms: false,
                     useBuiltIns: 'usage',
-                    corejs: '3.9',
+                    corejs: '3.10',
                 }
             ]
         ],
