65f129a4bbe390041cdfbf49407bd16b596a19b1
Michael Babker <michael.babker@gmail.com>
2021-05-27 18:54:52 -0500

    Add sponsors table/model

---
+++ app/Models/Sponsor.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace BabDev\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property int         $id
+ * @property string      $sponsorship_node_id
+ * @property bool        $is_public
+ * @property string      $sponsor_node_id
+ * @property string      $sponsor_username
+ * @property string|null $sponsor_display_name
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ *
+ * @property-read SponsorshipTier $sponsorship_tier
+ *
+ * @method static Builder|Sponsor query()
+ */
+class Sponsor extends Model
+{
+    protected $fillable = [
+        'sponsorship_node_id',
+        'is_public',
+        'sponsor_node_id',
+        'sponsor_username',
+        'sponsor_display_name',
+    ];
+
+    protected $casts = [
+        'is_public' => 'boolean',
+    ];
+
+    public function sponsorship_tier(): BelongsTo
+    {
+        return $this->belongsTo(SponsorshipTier::class);
+    }
+}
---
+++ database/migrations/2021_05_27_234944_create_sponsors_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSponsorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'sponsors',
+            function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('sponsorship_tier_id')->constrained()->onDelete('cascade');
+                $table->string('sponsorship_node_id')->unique();
+                $table->boolean('is_public');
+                $table->string('sponsor_node_id');
+                $table->string('sponsor_username');
+                $table->string('sponsor_display_name')->nullable();
+                $table->timestamps();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sponsors');
+    }
+}
