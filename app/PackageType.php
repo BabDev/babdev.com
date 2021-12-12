1749cd4556439f59a1df857a4fb7a8730dbc3633
Michael Babker <michael.babker@gmail.com>
2021-12-11 23:37:04 -0600

    Run CS fixer

--- app/PackageType.php
+++ app/PackageType.php
@@ -5,8 +5,12 @@ namespace BabDev;
 enum PackageType: string
 {
     case JOOMLA_EXTENSION = 'joomla-extension';
+
     case LARAVEL_PACKAGE = 'laravel-package';
+
     case PHP_PACKAGE = 'php-package';
+
     case SYLIUS_PLUGIN = 'sylius-plugin';
+
     case SYMFONY_BUNDLE = 'symfony-bundle';
 }
