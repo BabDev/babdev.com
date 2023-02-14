a071ae9fa2dd95b3e35dd4f245814b0f92450113
Michael Babker <michael.babker@gmail.com>
2023-02-13 20:38:19 -0500

    Refresh TinyMCE field assets

--- nova-components/TinyMCEField/webpack.mix.js
+++ nova-components/TinyMCEField/webpack.mix.js
@@ -1,10 +1,9 @@
 let mix = require('laravel-mix')
-let path = require('path')
 
-require('./mix')
+require('./nova.mix')
 
 mix
-  .setPublicPath('dist')
-  .js('resources/js/field.js', 'js')
-  .vue({ version: 3 })
-  .nova('babdev/tinymce-field')
+    .setPublicPath('dist')
+    .js('resources/js/field.js', 'js')
+    .vue({ version: 3 })
+    .nova('{babdev/tinymce-field')
