26c7d7e6b56f7db7ddee0ce7fe83941250179d70
Michael Babker <michael.babker@gmail.com>
2024-12-13 20:51:07 -0500

    Stable Rector versions

--- rector.php
+++ rector.php
@@ -3,25 +3,18 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
-use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
-use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\PostRector\Rector\NameImportingPostRector;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
-use RectorLaravel\Set\LaravelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/app',
         __DIR__ . '/config',
         __DIR__ . '/database',
         __DIR__ . '/resources',
         __DIR__ . '/routes',
         __DIR__ . '/tests',
-    ]);
-
-    $rectorConfig->skip([
+    ])
+    ->withSkip([
         /*
          * Skip selected rules
          */
@@ -29,26 +22,20 @@ return static function (RectorConfig $rectorConfig): void {
         /*
          * Skip rules for select files
          */
-        ChangeSwitchToMatchRector::class => [
-            // For readability, we don't want switch statements in these classes automatically being changed to match
-        ],
         NameImportingPostRector::class              => [
             __DIR__ . '/app/Http/Kernel.php',
             __DIR__ . '/config/*.php',
         ],
-        StringClassNameToClassConstantRector::class => [
-            // Rector tries to change the 'Event' label to Event::class
-            __DIR__ . '/config/app.php',
-        ],
-    ]);
-
-    $rectorConfig->importNames();
-    $rectorConfig->importShortClasses(false);
-
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_82,
-        SetList::CODE_QUALITY,
-        LaravelSetList::LARAVEL_110,
-        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-    ]);
-};
+    ])
+    ->withPreparedSets(codeQuality: true, phpunitCodeQuality: true)
+    ->withImportNames(importShortClasses: false)
+    ->withBootstrapFiles([
+        __DIR__ . '/vendor/larastan/larastan/bootstrap.php',
+    ])
+    ->withPHPStanConfigs([
+        __DIR__ . '/vendor/larastan/larastan/extension.neon',
+        __DIR__ . '/vendor/phpstan/phpstan-mockery/extension.neon',
+        __DIR__ . '/vendor/phpstan/phpstan-phpunit/extension.neon',
+        __DIR__ . '/phpstan.neon',
+    ])
+;
