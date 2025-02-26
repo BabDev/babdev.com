733b44f8a07a12e10a588cf0b81e71df2e49d260
Michael Babker <michael.babker@gmail.com>
2025-02-25 20:40:06 -0500

    Updates

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -64,9 +64,7 @@ return new PhpCsFixer\Config()
         'no_unused_imports' => true,
         'no_useless_else' => true,
         'no_useless_return' => true,
-        'nullable_type_declaration_for_default_null_value' => [
-            'use_nullable_type_declaration' => true,
-        ],
+        'nullable_type_declaration_for_default_null_value' => true,
         'ordered_imports' => [
             'imports_order' => ['class', 'function', 'const'],
             'sort_algorithm' => 'alpha',
