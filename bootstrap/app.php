ffbaaba4423d0e0fcefd3ec5ec2fa4286b6bcd3e
Michael Babker <michael.babker@gmail.com>
2025-03-09 00:50:25 -0500

    We don't need to pull downloads hourly

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,7 +21,7 @@ return Application::configure(basePath: \dirname(__DIR__))
     )
     ->withSchedule(function (Schedule $schedule): void {
         $schedule->command(\Spatie\GoogleFonts\Commands\FetchGoogleFontsCommand::class)->weekly();
-        $schedule->command(\BabDev\Console\Commands\ImportPackagistDownloads::class)->hourly();
+        $schedule->command(\BabDev\Console\Commands\ImportPackagistDownloads::class)->everyFourHours(24);
         $schedule->command(\BabDev\Console\Commands\ImportGitHubRepositories::class)->dailyAt('12:00');
         $schedule->command(\BabDev\Console\Commands\ImportGitHubSponsorshipTiers::class)->dailyAt('13:00');
         $schedule->command(\BabDev\Console\Commands\ImportGitHubSponsors::class)->dailyAt('13:30');
