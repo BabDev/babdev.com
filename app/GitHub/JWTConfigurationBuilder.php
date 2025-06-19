e298c0db3e82995cf43c28ffed90ccea7636037f
Michael Babker <michael.babker@gmail.com>
2025-06-18 21:12:35 -0400

    SA fix

--- app/GitHub/JWTConfigurationBuilder.php
+++ app/GitHub/JWTConfigurationBuilder.php
@@ -4,7 +4,6 @@ namespace App\GitHub;
 
 use App\Contracts\GitHub\Actions\Action;
 use App\Contracts\GitHub\JWTConfigurationBuilder as JWTConfigurationBuilderContract;
-use Illuminate\Support\Arr;
 use Lcobucci\JWT\Configuration;
 use Lcobucci\JWT\Signer\Key\InMemory;
 use Lcobucci\JWT\Signer\Rsa\Sha256;
@@ -21,7 +20,7 @@ final class JWTConfigurationBuilder implements JWTConfigurationBuilderContract
     {
         return Configuration::forSymmetricSigner(
             new Sha256(),
-            InMemory::file(Arr::string($repoConfig, 'key')),
+            InMemory::file($repoConfig['key']),
         );
     }
 }
